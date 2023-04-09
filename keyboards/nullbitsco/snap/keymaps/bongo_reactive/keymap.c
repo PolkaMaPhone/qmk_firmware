8f982f29ad5d9f8ea21388d05f92e95da00515f3
= <=>
2023-04-09 14:47:40 -0400

    refactor RGB config

--- keyboards/nullbitsco/snap/keymaps/bongo_reactive/keymap.c
+++ keyboards/nullbitsco/snap/keymaps/bongo_reactive/keymap.c
@@ -30,7 +30,7 @@ enum layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_all(
              KC_ESC,   KC_F1,   KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,    KC_F7,  KC_F8,  KC_F9,   KC_F10,    KC_F11,    KC_F12,   KC_PSCR,  KC_PAUS,
-    KC_MUTE, KC_GRV,   KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,     KC_7,   KC_8,   KC_9,    KC_0,      KC_MINS,   KC_EQL,   KC_BSPC,  KC_DEL,   KC_F13,
+    KC_MUTE, KC_GRV,   KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,     KC_7,   KC_8,   KC_9,    KC_0,      KC_MINS,   KC_EQL,   KC_BSPC,  KC_DEL,   ENC_RGB_TOG,
     KC_NO,   KC_TAB,   KC_Q,    KC_W,   KC_E,   KC_R,           KC_T,     KC_Y,   KC_U,   KC_I,    KC_O,      KC_P,      KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_HOME,
     KC_F15,  KC_CAPS,  KC_A,    KC_S,   KC_D,   KC_F,           KC_G,     KC_H,   KC_J,   KC_K,    KC_L,      KC_SCLN,   KC_QUOT,  KC_ENT,             KC_END,
     KC_F16,  KC_LSFT,  KC_NUHS, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,     KC_N,   KC_M,   KC_COMM, KC_DOT,    KC_SLSH,   KC_RSFT,            KC_UP,    KC_DEL,
@@ -56,7 +56,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // clang-format on
 
 #if defined(ENCODER_MAP_ENABLE)
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {[_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_F19, KC_F18)}, [_VIA1] = {ENCODER_CCW_CW(KC_BRID, KC_BRIU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT)}, [_VIA2] = {ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_WBAK, KC_WFWD)}};
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    // layer 0
+    [_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(RGB_RMOD, RGB_MOD)},
+    // layer 1
+    [_VIA1] = {ENCODER_CCW_CW(KC_BRID, KC_BRIU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT)},
+    //  layer 2
+    [_VIA2] = {ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_WBAK, KC_WFWD)}};
 #endif
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
@@ -64,6 +70,18 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
         return OLED_ROTATION_0;
     else
         return OLED_ROTATION_180;
+}
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) { // The first encoder (left side)
+        if (clockwise) {
+            rgblight_step_noeeprom(); // Cycle through animations when turned clockwise
+        } else {
+            rgblight_step_reverse_noeeprom(); // Cycle through animations in reverse when turned counterclockwise
+        }
+    }
+    // Add handling for the second encoder (right side), if necessary
+    return true;
 }
 
 static void render_status(void) {
@@ -133,31 +151,13 @@ bool oled_task_user(void) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     bongo_process_record(record);
     switch (keycode) {
-        case KC_F13:
+        case ENC_RGB_TOG:
             if (record->event.pressed) {
-                // when keycode KC_F13 is pressed
                 rgblight_toggle_noeeprom();
-            } else {
-                // when keycode KC_F13 is released
             }
-        case KC_F18:
-            if (record->event.pressed) {
-                // when keycode KC_F18 is pressed
-                rgblight_step_noeeprom();
-            } else {
-                // when keycode KC_F18 is released
-            }
-            break;
-        case KC_F19:
-            if (record->event.pressed) {
-                // when keycode KC_F19 is pressed
-                rgblight_step_reverse_noeeprom();
-            } else {
-                // when keycode KC_F19 is released
-            }
-            break;
+            return false; // Skip all further processing of this key
         default:
-            break;
+            return true; // Process all other keycodes normally
     }
     return true;
 }
